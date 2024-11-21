ad33bf0ac741d411ff8062c4acea9f9799b0f65a
yoshi <glodykabengele55@gmail.com>
2024-11-21 20:28:06 +0100

    first use with hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <h1 class="p-4 font-bold">Liste de pokemons</h1>
   `,
-  styles: [],
 })
-export class AppComponent {
-  title = 'firstAppAngular';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList)
+    this.selectPokemon(this.pokemonList[0])
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`)
+  }
 }
